a3b40bd19f1daa4881426ab276709a913485e18b
Bastian545 <97899801+Bastian545@users.noreply.github.com>
2022-11-17 16:34:39 -0300

    Update AppConstants.kt

--- app/src/main/java/com/bsoto/retrofitmovies/utils/AppConstants.kt
+++ app/src/main/java/com/bsoto/retrofitmovies/utils/AppConstants.kt
@@ -3,6 +3,6 @@ package com.bsoto.retrofitmovies.utils
 object AppConstants {
     //API
     const val BASE_URL="https://api.themoviedb.org/3/movie/"
-    const val API_KEY="f43e96acc2ada539a252aa92dc48b5ba"
+    const val API_KEY="-"
 
 }
